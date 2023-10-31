612fbd8ca23db1c50169ce188b15f310d4909a92
saiotest <elyermad@gmail.com>
2023-10-31 10:35:58 -0300

    new file to play Java

---
+++ src/test/java/e2e/steps/practicingJavaTest.java
@@ -0,0 +1,14 @@
+package e2e.steps;
+
+import org.junit.jupiter.api.*;
+
+public class practicingJavaTest {
+    // todo: */ Estructura de declaración de variables:
+    public static final String name = "Sai";
+
+    @Test
+    public void methodito() {
+        System.out.print("Hola Selenium Java");
+
+    }
+}
