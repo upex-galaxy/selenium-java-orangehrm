a08c0e5885f67ff444663111871c28ffb024100d
Nathalie Campos Marin <ncampoma@emeal.nttdata.com>
2023-11-01 13:47:44 -0300

    test(NaTest): practicando git

---
+++ src/test/java/e2e/steps/example/NaTest.java
@@ -0,0 +1,13 @@
+package e2e.steps.example;
+
+public class NaTest {
+    public static void main(String[] args) {
+        int n1 = 10;
+        double n2 = 20.59;
+        String name = "Nathalie";
+        String last = "Campos";
+
+        System.out.println("La suma de " + n1 + " mas " + n2 + " es igual a : " + (n1 + n2));
+        System.out.println("Estudiante: " + name + " " + last);
+    }
+}
