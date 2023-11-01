42d02b5d0da8f5577de26d7fe0e1403db21aaf79
Daryelin Rossell <daryelin.rossell@gmail.com>
2023-11-01 17:52:18 -0400

    test(daryeTest): cambiando funcion main por TEST

--- src/test/java/e2e/steps/examples/daryeTest.java
+++ src/test/java/e2e/steps/examples/daryeTest.java
@@ -1,18 +1,18 @@
 
 package e2e.steps.examples;
+import org.junit.jupiter.api.*;
 
 public class daryeTest {
-    public static void main(String[] args) {
-        sumarNumeros(42, 56.78);
-        nombreCompleto("Daryelin", "Rossell");
-    }
-
-    public static void sumarNumeros(int numeroEntero, double numeroFlotante) {
+    public static final String nombre = "Daryelin";
+    public static final String apellido = "Rossell";
+    public static final Integer numeroEntero = 42;
+    public static final Double numeroFlotante = 56.78;
+    
+    @Test
+    public void miPrimerMetodoJava() {
+        System.out.println("Mi nombre completo es: " + nombre + " " + apellido);
         double suma = numeroEntero + numeroFlotante;
         System.out.println("La suma es: " + suma);
     }
 
-    public static void nombreCompleto(String nombre, String apellido) {
-        System.out.println("Mi nombre completo es: " + nombre + " " + apellido);
-    }
 }
