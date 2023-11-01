0c0f5227180f28d2458e404199b4a4f62098840e
lorena tamayo <cottalorena@gmail.com>
2023-11-01 17:55:19 +0100

    example for course

---
+++ src/test/java/e2e/steps/example/LorenaTest.java
@@ -0,0 +1,15 @@
+package e2e.steps.example;
+
+public class LorenaTest {
+    // mision 1: sumar 2 variables(int y float)
+    // 2: concatenar 2 string
+    // crear un metodo para hacer esto
+    // imprimir los resultados en consola con system.out.print
+
+    public static void main(String[] args) {
+        int numeroEntero = 10; // Variable de tipo int
+        float numeroDecimal = 3.14f; // Variable de tipo float
+        float resultado = numeroEntero + numeroDecimal;
+        System.out.println(resultado);
+    }
+}
