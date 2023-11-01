e489ec8fb1bb3b624938df4b619fbb7c5a4ef645
saiotest <elyermad@gmail.com>
2023-11-01 10:11:58 -0300

    test(ElyTest): created a new challenge for course

---
+++ src/test/java/e2e/steps/example/ElyTest.java
@@ -0,0 +1,11 @@
+package e2e.steps.example;
+
+public class ElyTest {
+
+    // MISSION 1: Sumar 2 variables (int y float)
+    // MISSION 2: Concatenar 2 String (Nombre y Apellido) "" + ""
+
+    // Crear un Método para hacer eso.
+
+    // Imprimir los resultados en la Consola con System.out.print.
+}
