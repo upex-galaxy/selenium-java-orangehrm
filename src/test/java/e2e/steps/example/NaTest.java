b5d5590eb686add4199fd5aacce229b348a865ad
Nathalie Campos Marin <ncampoma@emeal.nttdata.com>
2023-11-01 21:39:26 -0300

    cambio método main por test

--- src/test/java/e2e/steps/example/NaTest.java
+++ src/test/java/e2e/steps/example/NaTest.java
@@ -1,7 +1,10 @@
 package e2e.steps.example;
 
+import org.junit.jupiter.api.*;
+
 public class NaTest {
-    public static void main(String[] args) {
+    @Test
+    public void practicandoGit() {
         int n1 = 10;
         double n2 = 20.59;
         String name = "Nathalie";
