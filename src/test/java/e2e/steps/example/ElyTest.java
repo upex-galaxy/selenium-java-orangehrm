44e33201526d2df0431c383f16fd321596baf350
saiotest <elyermad@gmail.com>
2023-11-01 21:03:05 -0300

    agregué un poquito de sazón a la sopa del Challenge

--- src/test/java/e2e/steps/example/ElyTest.java
+++ src/test/java/e2e/steps/example/ElyTest.java
@@ -2,10 +2,12 @@ package e2e.steps.example;
 
 public class ElyTest {
 
-    // MISSION 1: Sumar 2 variables (int y float)
+    // MISSION 1: Sumar 2 variables (Int y Double)
     // MISSION 2: Concatenar 2 String (Nombre y Apellido) "" + ""
 
     // Crear un Método para hacer eso.
 
     // Imprimir los resultados en la Consola con System.out.print.
+
+    // Guardar cambios, git add, git commit, y git push => y abrir Pull Request.
 }
