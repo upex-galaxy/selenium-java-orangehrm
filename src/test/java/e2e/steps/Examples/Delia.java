ba87bba8c835a0263eda651ad2514926fb5a92e9
Your NameDeliaCardenas <delia_cardenas_30@hotmail.com>
2023-11-02 16:33:44 -0300

    setting.json modificado y archivo de tarea agregado

---
+++ src/test/java/e2e/steps/Examples/Delia.java
@@ -0,0 +1,20 @@
+package e2e.steps.Examples;
+
+import org.junit.Test;
+
+public class Delia {
+
+    String Nombre = "Delia";
+    String Apellido = "Cardenas";
+    int primer_Numero = 10;
+    double Segundo_Numero = 5.34;
+
+    @Test
+    public void tarea() {
+        System.out.println("Mi nombre completo es " + Nombre + " " + Apellido);
+        double suma = primer_Numero + Segundo_Numero;
+        System.out.println("el resultado es :" + suma);
+
+    }
+
+}
