90b803055cc53d2a038eb11865b935a2058593ca
ramostaime <ramostaime@gmail.com>
2023-11-05 09:35:18 -0300

    test(TayTest): created methods

--- src/test/java/e2e/steps/example/TayTest.java
+++ src/test/java/e2e/steps/example/TayTest.java
@@ -3,10 +3,17 @@ package e2e.steps.Example;
 import org.junit.jupiter.api.*;
 
 public class TayTest {
+    private Integer num1 = 1;
+    private Double num2 = 0.3;
+
+    public Double getNumber(Integer pNum1, Double pNum2) {
+        return pNum1 + pNum2;
+
+    }
 
     @Test
     public void practiceGit() {
-
+        // getNumber()
     }
 
 }
