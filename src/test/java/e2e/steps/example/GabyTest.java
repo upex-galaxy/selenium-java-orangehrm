b3247e6a89d49ab6a7c5e22b58e96349281f25d2
Gabriela Arocha <mgabrielaarocha@gmail.com>
2023-11-05 14:28:43 -0300

    test(GabyTest): New challenge to do

---
+++ src/test/java/e2e/steps/example/GabyTest.java
@@ -0,0 +1,17 @@
+package e2e.steps.example;
+
+public class GabyTest {
+    public class SumaVariables {
+        public static void main(String[] args) {
+            // Declarar dos variables: una int y una float
+        int numeroEntero = 7;
+        float numeroDecimal = 7.5f;
+
+        // Realizar la suma
+        float resultado = numeroEntero + numeroDecimal;
+
+        
+        System.out.println("La suma es: " + resultado);
+    }
+}  
+}
