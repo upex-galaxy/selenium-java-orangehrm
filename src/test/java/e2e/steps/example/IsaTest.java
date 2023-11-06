3bb3bf3ab65c4a0510ef6959aeb74a01959248db
igflores <139146133+igflores@users.noreply.github.com>
2023-11-06 07:17:30 -0800

    (IsaTest): Added the methods to make the sum using random values and to concatenate two String using random values too

--- src/test/java/e2e/steps/example/IsaTest.java
+++ src/test/java/e2e/steps/example/IsaTest.java
@@ -1,20 +1,41 @@
+// Sumar 2 variables (Int y Double)
+// Concatenar 2 String (nombre y apellido)
+// Crear un Método para hacer eso.
+// Imprimir los resultados en la Consola con System.out.print.
+// Guardar cambios, git add, git commit, y git push => y abrir Pull Request.
+
 package e2e.steps.example;
 import org.junit.jupiter.api.*;
+import java.util.Random;
+import org.junit.Test;
 
 public class IsaTest {
+    private static final String[] names = {"Isabel", "Emily", "Jamie", "David", "Alexander"};
+    private static final String[] lastNames = {"Gonzalez", "Gil", "Flores", "Gomez", "Perez"};
+
     @Test
-    public static void ValuesToPrint() {        
-        SumNumbers(10, 10.5);
-        ConcatenateName("Isabel", "Gonzalez");
-    }
-    
-    public static void SumNumbers(int integerNumber, double doubleNumber){
-        Double sumResult = firstNumber + secondNumber;
+    public void testPrintSumNumbers() {
+        int integerNumber = new Random().nextInt(100);
+        double doubleNumber = new Random().nextDouble() * 100;
+
+        Double sumResult = SumNumbers(integerNumber, doubleNumber); //call the method and get the result
         System.out.println("The result of the sum is: " + sumResult);
     }
 
-    public static void ConcatenateName(String name, String lastName) {
-        String concatenateString = name + " " + lastName;
+    @Test
+    public void testPrintConcatenateName() {
+        String name = names[new Random().nextInt(names.length)];
+        String lastName = lastNames[new Random().nextInt(lastNames.length)];
+
+        String concatenateString = ConcatenateName(name, lastName); //call the method and get the result
         System.out.println("The full name is: " + concatenateString);
+    }
+
+    public Double SumNumbers(int integerNumber, double doubleNumber) {
+        return integerNumber + doubleNumber;
+    }
+
+    public String ConcatenateName(String name, String lastName) {
+        return name + " " + lastName;
     }
 }
