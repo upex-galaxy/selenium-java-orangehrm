83d6082c6a5bfc1a3ffa6584e29b578005031d61
Gabriela Arocha <mgabrielaarocha@gmail.com>
2023-11-05 22:42:35 -0300

    git

--- src/test/java/e2e/steps/example/GabyTest.java
+++ src/test/java/e2e/steps/example/GabyTest.java
@@ -1,17 +1,34 @@
 package e2e.steps.example;
 
+import org.junit.jupiter.api.*;
 public class GabyTest {
-    public class SumaVariables {
-        public static void main(String[] args) {
-            // Declarar dos variables: una int y una float
-        int numeroEntero = 7;
-        float numeroDecimal = 7.5f;
+    // Declarar variables: una int, un double, dos String
+    public static Integer numeroEntero = 7;
+    public static double numeroDecimal = 7.5;
+    public static String nombre = "Gabriela";
+    public static String apellido = "Aro";
+    // !Llamar al método PrimerChallenge // para realizar la suma y concatenación
+    @Test
+    public static void PrimerChallenge(Integer entero, double decimal, String nombre, String apellido) {
+        //! Suma de las variables numéricas
+        double resultadoSuma = (double) numeroEntero + numeroDecimal;
+        //! Concatenar las cadenas de texto
+        String nombreCompleto = nombre + " " + apellido;
+        //! Imprimir resultados en la consola
 
-        // Realizar la suma
-        float resultado = numeroEntero + numeroDecimal;
+        System.out.println("La suma de los números es: " + resultadoSuma);
+        System.out.println("El nombre completo es: " + nombreCompleto);
+    }   
+}
+
+
+    // public double getNumber(Integer firstNum, double secondNum) {
+    // return firstNum + secondNum;
+    // }
+    //Double resultadoSuma = getNumber(numeroEntero, numeroDecimal);
+    //System.out.print(resultadoSuma);
+    //PrimerChallenge(numeroEntero, numeroDecimal, nombre, apellido);
+    //public static void PrimerChallenge(int entero, float decimal, String nombre, String apellido) {
+    //int resultadoSuma = entero + (int) decimal; // Se convierte el float a int para la suma
 
         
-        System.out.println("La suma es: " + resultado);
-    }
-}  
-}
